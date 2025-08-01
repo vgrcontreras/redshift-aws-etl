b317a1b713ecb7dc2ee614bb376f96129ff7e74e
Victor Contreras <contreras3991@gmail.com>
2025-08-01 17:24:09 -0300

    add boto3 client

--- utils/aws_clients.py
+++ utils/aws_clients.py
@@ -8,9 +8,18 @@ AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 REGION_NAME = os.getenv('REGION_NAME')
 
-s3_client = boto3.resource(
+s3_resource = boto3.resource(
     's3',
     aws_access_key_id=AWS_ACCESS_KEY_ID,
     aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
     region_name=REGION_NAME
 )
+
+s3_client = boto3.client(
+    's3',
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    region_name=REGION_NAME
+)
+
+
